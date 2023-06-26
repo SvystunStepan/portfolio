2eb86530d5c8ad54d4d87b7836004b3fdee2a610
Stepan Svystun <s916ua@gmail.com>
2023-06-26 13:56:14 +0300

    *

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+
+  // $('.slider').slick();
+
+  /* $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    arrows: true,
+    dots: false,
+    swipe: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    focusOnSelect: true
+  }); */
+
+  /* $(window).scroll(function () {
+    return $('.nav').toggleClass("fixed", $(window).scrollTop() > 0);
+  }); */
+
+  
+  // Scroll
+  /* $('.scroll').click(function (e) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+  
+    $('body,html').animate({
+      scrollTop: top - 90
+    }, 500);
+  
+  }); */
+  
+
+}) 
