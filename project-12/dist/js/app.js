b2a9c299aad566aabf3d13984b1e46ef32047bf3
Stepan Svystun <s916ua@gmail.com>
2023-07-24 14:08:23 +0300

    commit

--- project-12/dist/js/app.js
+++ project-12/dist/js/app.js
@@ -1,74 +1,94 @@
-/**
- * jQuery-viewport-checker - v1.8.8 - 2017-09-25
- * https://github.com/dirkgroenen/jQuery-viewport-checker
- *
- * Copyright (c) 2017 Dirk Groenen
- * Licensed MIT <https://github.com/dirkgroenen/jQuery-viewport-checker/blob/master/LICENSE>
- */
-
- !function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery);
+!function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery);
 
 $( document ).ready(function() {
 
-  if ($(window).width() > 1006) {
-    /* $('.title span').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated pulse', // Class to add to the elements when they are visible
-      offset: "10%"
-    }); */
-    $('.head .container').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated slideInUp', 
-      offset: 0
-    });
-  }
-    $('.right').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated fadeInRight', 
-      offset: '0%'
-    });
-    $('.left').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated fadeInLeft', 
-      offset: '0%'
-    });
-    $('.up').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated fadeInUp', 
-      offset: '30%'
-    });
-    $('.down').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated fadeInDown', 
-      offset: '30%'
-    });
-    $('.in').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated fadeIn', 
-      offset: '30%'
-    });
-    $('.bounce-in').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated bounceIn', 
-      offset: '30%'
-    });
-    $('.bounce-left').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated bounceInLeft', 
-      offset: '30%'
-    });
-    $('.bounce-right').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated bounceInRight', 
-      offset: '30%'
-    });
-    $('.bounce-up').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated bounceInUp', 
-      offset: '30%'
-    });
-    $('.bounce-down').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated bounceInDown', 
-      offset: '30%'
-    });
-    $('.speaker-img').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated zoomIn', 
-      offset: '0%'
-    });
-    $('.bottom-info').addClass("hidden_animation").viewportChecker({
-      classToAdd: 'visible animated rotateIn', 
-      offset: '30%'
-    });
-  // }
+  $('.head').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated slideInUp', 
+    offset: '0%'
+  });
+  $('.right').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated fadeInRight', 
+    offset: '0%'
+  });
+  $('.left').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated fadeInLeft', 
+    offset: '0%'
+  });
+  $('.up').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated fadeInUp', 
+    offset: '20%'
+  });
+  $('.down').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated fadeInDown', 
+    offset: '20%'
+  });
+  $('.in').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated fadeIn', 
+    offset: '20%'
+  });
+  $('.bounce-in').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceIn', 
+    offset: '20%'
+  });
+  $('.bounce-left').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceInLeft', 
+    offset: '20%'
+  });
+  $('.bounce-right').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceInRight', 
+    offset: '20%'
+  });
+  $('.bounce-up').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceInUp', 
+    offset: '20%'
+  });
+  $('.bounce-down').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceInDown', 
+    offset: '20%'
+  });
+  $('.speaker-img').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated zoomIn', 
+    offset: '0%'
+  });
+  $('.bottom-info').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated rotateIn', 
+    offset: '20%'
+  });
+
+  //фіксована кнопка desctop 
+  window.addEventListener('scroll', function() {
+    var a = document.querySelector('.banner-float-btn');
+    var priceBlock = document.getElementById('price');
+    var bannerBlock = document.getElementById('banner');
+    var scrollY = window.scrollY;
+    var priceBlockTop = priceBlock.offsetTop;
+    var priceBlockHeight = priceBlock.offsetHeight;
+    var priceBlockBottom = priceBlockTop + priceBlockHeight;
+    var bannerBlockTop = bannerBlock.offsetTop;
+    var bannerBlockHeight = bannerBlock.offsetHeight;
+    var bannerBlockBottom = bannerBlockTop + bannerBlockHeight;
+    
+    //стає на місце при знаходженні на блоці "banner"
+    if (scrollY < bannerBlockBottom) {
+      a.style.position = 'static';
+      a.style.bottom = '0px';
+      a.style.left = '0';
+      a.style.transform = 'translateX(0%)';
+    } else {
+      a.style.position = 'fixed';
+      a.style.top = 'auto';
+      a.style.bottom = '20px';
+      a.style.left = '50%';
+      a.style.transform = 'translateX(-50%)';
+    }
+
+    //зникає при знаходженні на блоці "price"
+    if (scrollY > priceBlockTop && scrollY < priceBlockBottom) {
+      a.style.display = 'none';
+    } else {
+      a.style.display = 'block';
+    }
+  });
 
   // Scroll
   $('.scroll').click(function (e) {
@@ -81,7 +101,6 @@ $( document ).ready(function() {
     }, 1500);
   });
 
-
   //головний слайдер фото
   $('#slider1').slick({
     dots: true,
@@ -89,8 +108,7 @@ $( document ).ready(function() {
     speed: 2000,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 5000
-    // adaptiveHeight: true
+    autoplaySpeed: 3000
   });
 
   //слайдер телеканали
@@ -115,75 +133,14 @@ $( document ).ready(function() {
     dots: true,
     speed: 1000,
     slidesToShow: 1,
-    // slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 5000,
-    // adaptiveHeight: true
+    adaptiveHeight: true,
     arrows: true,
     responsive: [{
       breakpoint: 768,
       settings: {
-        arrows: false
+        speed: 500,
+        arrows: false,
       }
     }]
   });
-
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    //сдайдер модулів
-    $('#slider4').slick({
-      dots: true,
-      arrows: false,
-      speed: 500,
-      slidesToShow: 1,
-      // autoplay: true,
-      // autoplaySpeed: 5000,
-      adaptiveHeight: true
-    });
-
-    //сдайдер бонусів
-    $('#slider5').slick({
-      dots: true,
-      arrows: false,
-      speed: 500,
-      slidesToShow: 1,
-      // autoplay: true,
-      // autoplaySpeed: 5000,
-      adaptiveHeight: true
-    });
-
-    //фіксована кнопка мобіла 
-    window.addEventListener('scroll', function() {
-      var a = document.querySelector('.floating-button');
-      var priceBlock = document.getElementById('price');
-      var scrollY = window.scrollY;
-      var priceBlockTop = priceBlock.offsetTop;
-      var priceBlockHeight = priceBlock.offsetHeight;
-      var priceBlockBottom = priceBlockTop + priceBlockHeight;
-      
-      if (scrollY > 0) {
-        a.style.top = 'auto';
-        a.style.bottom = '20px';
-      } else {
-        a.style.top = '450px';
-        a.style.bottom = 'auto';
-      }
-      //зникає при знаходженні на блоці "price"
-      if (scrollY > priceBlockTop && scrollY < priceBlockBottom) {
-        a.style.display = 'none';
-      } else {
-        a.style.display = 'block';
-      }
-
-    });
-
-  }
-  
-
-  /* if (/Mobi|Android/i.test(navigator.userAgent)) {
-    // Код, який виконується на мобільних пристроях
-  } else {
-    // Код, який виконується на інших пристроях
-  } */
 }) 
-
-
