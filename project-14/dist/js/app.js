d4b0ccca374a8b98e9ee0414a1b3ebbf0825611d
Stepan Svystun <s916ua@gmail.com>
2023-10-17 22:42:27 +0300

    new project

--- project-14/dist/js/app.js
+++ project-14/dist/js/app.js
@@ -31,17 +31,32 @@ Zepto(function($) {
   })
 });
 
+
+/*! lozad.js - v1.9.0 - 2019-02-09
+* https://github.com/ApoorvSaxena/lozad.js
+* Copyright (c) 2019 Apoorv Saxena; Licensed MIT */
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?module.exports=e():"function"==typeof define&&define.amd?define(e):t.lozad=e()}(this,function(){"use strict";var g=Object.assign||function(t){for(var e=1;e<arguments.length;e++){var r=arguments[e];for(var o in r)Object.prototype.hasOwnProperty.call(r,o)&&(t[o]=r[o])}return t},n="undefined"!=typeof document&&document.documentMode,l={rootMargin:"0px",threshold:0,load:function(t){if("picture"===t.nodeName.toLowerCase()){var e=document.createElement("img");n&&t.getAttribute("data-iesrc")&&(e.src=t.getAttribute("data-iesrc")),t.getAttribute("data-alt")&&(e.alt=t.getAttribute("data-alt")),t.appendChild(e)}if("video"===t.nodeName.toLowerCase()&&!t.getAttribute("data-src")&&t.children){for(var r=t.children,o=void 0,a=0;a<=r.length-1;a++)(o=r[a].getAttribute("data-src"))&&(r[a].src=o);t.load()}t.getAttribute("data-src")&&(t.src=t.getAttribute("data-src")),t.getAttribute("data-srcset")&&t.setAttribute("srcset",t.getAttribute("data-srcset")),t.getAttribute("data-background-image")&&(t.style.backgroundImage="url('"+t.getAttribute("data-background-image")+"')"),t.getAttribute("data-toggle-class")&&t.classList.toggle(t.getAttribute("data-toggle-class"))},loaded:function(){}};
+/**
+   * Detect IE browser
+   * @const {boolean}
+   * @private
+   */function f(t){t.setAttribute("data-loaded",!0)}var b=function(t){return"true"===t.getAttribute("data-loaded")};return function(){var r,o,a=0<arguments.length&&void 0!==arguments[0]?arguments[0]:".lozad",t=1<arguments.length&&void 0!==arguments[1]?arguments[1]:{},e=g({},l,t),n=e.root,i=e.rootMargin,d=e.threshold,c=e.load,u=e.loaded,s=void 0;return window.IntersectionObserver&&(s=new IntersectionObserver((r=c,o=u,function(t,e){t.forEach(function(t){(0<t.intersectionRatio||t.isIntersecting)&&(e.unobserve(t.target),b(t.target)||(r(t.target),f(t.target),o(t.target)))})}),{root:n,rootMargin:i,threshold:d})),{observe:function(){for(var t=function(t){var e=1<arguments.length&&void 0!==arguments[1]?arguments[1]:document;return t instanceof Element?[t]:t instanceof NodeList?t:e.querySelectorAll(t)}(a,n),e=0;e<t.length;e++)b(t[e])||(s?s.observe(t[e]):(c(t[e]),f(t[e]),u(t[e])))},triggerLoad:function(t){b(t)||(c(t),f(t),u(t))},observer:s}}});
+
+   var observer = lozad();
+  observer.observe();
+
+
 !function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery)
 
- $( document ).ready(function() {
+$( document ).ready(function() {
 
-  // $('.head').addClass("hidden_animation").viewportChecker({
-  //   classToAdd: 'visible animated slideInUp',
-  //   offset: '0%'
-  // });
-  $('.header-img, .anime').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated zoomIn',
+  /* $('.head').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated slideInUp',
     offset: '0%'
+  }); */
+  $('.header-img, .speaker-img, .in-img, .reviews-img').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated zoomIn',
+    offset: '30%'
   });
   $('.up-head').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated fadeInUp',
@@ -57,88 +72,64 @@ Zepto(function($) {
   });
   $('.up-1').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated-1 fadeInUp',
-    offset: '10%'
+    offset: '20%'
   });
   $('.up-2').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated-2 fadeInUp',
-    offset: '10%'
+    offset: '20%'
   });
   $('.up-3').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated-3 fadeInUp',
-    offset: '10%'
+    offset: '20%'
   });
   $('.up-4').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated-4 fadeInUp',
-    offset: '10%'
+    offset: '20%'
   });
   $('.up-5').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated-5 fadeInUp',
-    offset: '10%'
-  });
-  $('.right-1').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-1 fadeInRight',
-    offset: '10%'
-  });
-  $('.right-2').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-2 fadeInRight',
-    offset: '10%'
-  });
-  $('.right-3').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-3 fadeInRight',
-    offset: '10%'
-  });
-  $('.right-4').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-4 fadeInRight',
-    offset: '10%'
-  });
-  $('.right-5').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-5 fadeInRight',
-    offset: '10%'
-  });
-  $('.left-1').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-1 fadeInLeft',
-    offset: '10%'
-  });
-  $('.left-2').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-2 fadeInLeft',
-    offset: '10%'
-  });
-  $('.left-3').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-3 fadeInLeft',
-    offset: '10%'
-  });
-  $('.left-4').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-4 fadeInLeft',
-    offset: '10%'
-  });
-  $('.left-5').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-5 fadeInLeft',
-    offset: '10%'
+    offset: '0%'
   });
   $('.down').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated fadeInDown',
     offset: '0%'
   });
-  $('.right').addClass("hidden_animation").viewportChecker({
+  $('.right, .top-10-block').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated fadeInRight',
+    offset: '20%'
+  });
+  $('.right-1').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated-1 fadeInRight',
+    offset: '20%'
+  });
+  $('.right-2').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated-2 fadeInRight',
     offset: '20%'
   });
   $('.left').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated fadeInLeft',
     offset: '20%'
   });
+  /* $('.left-1').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated-1 fadeInLeft',
+    offset: '20%'
+  });
+  $('.left-2').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated-2 fadeInLeft',
+    offset: '20%'
+  });
   $('.flip').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated flipInY',
     offset: '20%'
-  });
+  }); */
   $('.in').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated fadeIn',
+    classToAdd: 'visible animated-3 fadeIn',
     offset: '30%'
   });
-  // $('.bounce-in').addClass("hidden_animation").viewportChecker({
-  //   classToAdd: 'visible animated bounceIn',
-  //   offset: '20%'
-  // });
+  /* $('.bounce-in').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceIn',
+    offset: '20%'
+  }); */
   $('.bounce-left').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated-3 bounceInLeft',
     offset: '20%'
@@ -147,44 +138,18 @@ Zepto(function($) {
     classToAdd: 'visible animated-3 bounceInRight',
     offset: '20%'
   });
-  $('.bounce-up').addClass("hidden_animation").viewportChecker({
-    classToAdd: 'visible animated-1 bounceInUp',
+  /* $('.bounce-up').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceInUp',
     offset: '20%'
   });
-  // $('.bounce-down').addClass("hidden_animation").viewportChecker({
-  //   classToAdd: 'visible animated bounceInDown',
-  //   offset: '20%'
-  // });
+  $('.bounce-down').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated bounceInDown',
+    offset: '20%'
+  }); */
   $('.results').addClass("hidden_animation").viewportChecker({
     classToAdd: 'visible animated rotateIn',
     offset: '30%'
   });
-
-
-  // Burger-main
-  $('.drop-btn').click(function() {
-    let dropdown = document.getElementById("dropdown");
-    let container = document.querySelector(".blok");
-    dropdown.classList.toggle("show");
-    container.classList.toggle("change");
-  });
-
-  window.addEventListener("click", function(event) {
-    let dropdowns = document.getElementsByClassName("nav-main-box");
-    let containers = document.getElementsByClassName("blok");
-
-    if (!event.target.matches('.drop-btn') && !event.target.matches('.bar1') && !event.target.matches('.bar2') && !event.target.matches('.bar3') && !event.target.matches('.blok')) {
-      for (let i = 0; i <dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        let container = containers[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-          container.classList.remove('change');
-        }
-      }
-    }
-  });
-
 
   // Scroll
   $('.scroll').click(function (e) {
@@ -193,10 +158,9 @@ Zepto(function($) {
       top = $(id).offset().top;
 
     $('body,html').animate({
-      scrollTop: top - 50
+      scrollTop: top - -20
     }, 1500);
   });
-
 
   //головний слайдер фото
   $('#slider1').slick({
@@ -207,7 +171,6 @@ Zepto(function($) {
     autoplay: true,
     autoplaySpeed: 3000
   });
-
 
   //слайдер телеканали
   $('#slider2').slick({
@@ -230,34 +193,9 @@ Zepto(function($) {
   $('#slider3').slick({
     dots: false,
     speed: 1000,
-    slidesToShow: 3,
+    slidesToShow: 1,
     adaptiveHeight: true,
     arrows: true,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1
-      }
-    }]
-  });
-
-  //слайдер ЯК ВПЛИВАЮТЬ ЖОРСТКІ ПОКАРАННЯ
-  $(document).ready(function () {
-    if ($('#slider4').css('display') === 'block') {
-      $('#slider4').slick({
-        // Налаштування Slick Slider для мобільних пристроїв
-        responsive: [{
-          breakpoint: 768,
-          settings: {
-          slidesToShow: 1,
-          dots: true,
-          arrows: true,
-          speed: 1000,
-          adaptiveHeight: true
-          }
-        }]
-      });
-    }
   });
 
   //accardion
